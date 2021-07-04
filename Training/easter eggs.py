92ae60a96bbadbe09af6f96f04f0ffd1a41bbab7
Paulina <79440830+PauPie74@users.noreply.github.com>
2021-07-04 22:09:40 +0200

    Created Training

---
+++ Training/easter eggs.py
@@ -0,0 +1,14 @@
+import antigravity
+
+import this
+
+
+# Sys exit
+import sys
+
+while True:
+    print("Type 'exit' to exit")
+    response = input()
+    if response == "exit":
+        print("Exiting the program")
+        sys.exit()
